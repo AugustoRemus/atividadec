17f3debfccbfc4420d2737e4636983fe9fcc9464
Augusto Renostro Remus <131462760+AugustoRemus@users.noreply.github.com>
2023-09-19 17:24:59 -0300

    Update atividade1recursividade.c

--- atividade1recursividade.c
+++ atividade1recursividade.c
@@ -3,11 +3,11 @@
 int func(int base, int expoente, int total);
 
 int main(){
-int total = 1;
-int expoente = 1;
-int base = 4;
-total = func(base, expoente, total);
-printf("%d", total);
+    int total = 1;
+    int expoente = 1;
+    int base = 4;
+    total = func(base, expoente, total);
+    printf("%d", total);
 }
 
 int func(int base, int expoente, int total)
